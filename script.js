24c8789bcd6ff022604fd43ecb73641fe732ec05
Nasir Tomlin <126700689+NasirTomlin@users.noreply.github.com>
2023-12-20 16:01:27 -0500

    Add files via upload
    implemented light and dark mode

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ function toggleMenu()
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function darkmode()
+{
+    var icon = document.getElementById("icon");
+    icon.onclick = function()
+    {
+        document.body.classList.toggle("dark-theme");
+        if(document.body.classList.contains("dark-theme"))
+        {
+            icon.src = "./assets/moon.png";
+        }else{
+            icon.src = "./assets/sun.png";
+        }
+    }
+}
